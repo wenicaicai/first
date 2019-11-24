175634c8d0030cd363a6fdc65863708a62783861
杨永财 <youngwenicai@outlook.com>
2019-11-24 23:31:27 +0800

    仓库层

---
+++ Domain/IRepositories/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.IRepositories
+{
+    public interface IRepository<TEntity> where TEntity : AggregateRoot
+    {
+        #region 属性
+        IQueryable<TEntity> Entities { get; }
+        #endregion
+
+        #region 方法
+        int Insert(TEntity entity);
+
+        int Insert(IEnumerable<TEntity> entities);
+
+        int Delete(object id);
+
+        int Delete(TEntity entity);
+
+        int Delete(IEnumerable<TEntity> entities);
+
+        TEntity GetByKey(object key);
+
+        #endregion
+    }
+}
